93ea016a12815a9407fb32e3d0c603124417f3fa
root <root@ip-172-31-16-245.us-west-2.compute.internal>
2013-07-16 17:53:23 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,22 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var content;
+
+fs.readFile('./index.html', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+app.get('/', function(request, response) {
+  response.send(content);
+});
+
+    // Invoke the next step here however you like
+  //  console.log(content);   // Put all of the code here (not the best solution)
+ //   processFile();          // Or put the next step in a function and invoke it
+});
+
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });

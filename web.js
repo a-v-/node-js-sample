e1e8ec8dc3091c76024e6cc1cf52a5293528fcc2
root <root@ip-172-31-16-245.us-west-2.compute.internal>
2013-07-16 18:27:39 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,9 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buf = new Buffer (fs.readFileSync('index.html'))
-//  buff = fs.readFileSync('index.html')  
-  response.send(buf.toString());
+  var buffer = new Buffer (fs.readFileSync('index.html'))
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
